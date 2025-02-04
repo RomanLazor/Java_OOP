73d3fec51749e355d6becf0a7bb308160c871904
RomanLazor <sasredemption123@gmail.com>
2025-02-04 12:37:22 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Practice fr = new Practice(10.5, 10);
+        Practice fr2 = new Practice(20, 21.5);
+        double s = fr.length * fr.width;
+        double s2 = fr2.length * fr2.width;
+
+        System.out.println(s);
+        System.out.println(s2);
     }
 }
